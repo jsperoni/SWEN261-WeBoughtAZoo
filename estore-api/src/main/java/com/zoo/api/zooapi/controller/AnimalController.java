3865a908729ccd9af0a94625a7a3636696647ca6
joe <apjoey914@gmail.com>
2023-02-19 15:57:43 -0500

    This does not deserve a PR. I am going clinically insane.'

--- estore-api/src/main/java/com/zoo/api/zooapi/controller/AnimalController.java
+++ estore-api/src/main/java/com/zoo/api/zooapi/controller/AnimalController.java
@@ -193,7 +193,17 @@ public class AnimalController {
     public ResponseEntity<Animal> deleteAnimal(@PathVariable int id) {
         LOG.info("DELETE /animals/" + id);
 
-        // Replace below with your implementation
-        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+        try {
+
+            if (animalDao.deleteAnimal(id))
+                return new ResponseEntity<>(HttpStatus.OK);
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
     }
 }
