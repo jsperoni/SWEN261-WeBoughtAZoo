cbe79c9ab579e88785713cac782c313c3f67af66
joe <apjoey914@gmail.com>
2023-03-05 14:24:06 -0500

    Add CustomerFileDAO constructor

--- estore-api/src/main/java/com/zoo/api/zooapi/persistence/CustomerFileDAO.java
+++ estore-api/src/main/java/com/zoo/api/zooapi/persistence/CustomerFileDAO.java
@@ -1,43 +1,73 @@
 package com.zoo.api.zooapi.persistence;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zoo.api.zooapi.model.Customer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.logging.Logger;
 
 @Component
 public class CustomerFileDAO implements CustomerDAO {
     private static final Logger LOG = Logger.getLogger(CustomerFileDAO.class.getName());
 
+    Map<Integer, Customer> customers;
+
+    private ObjectMapper objectMapper;
+    private String filename;
+
+    private static int nextId;
+
+    public CustomerFileDAO(@Value("data/customers.json") String filename, ObjectMapper objectMapper) throws IOException {
+        this.filename = filename;
+        this.objectMapper = objectMapper;
+        load();  // load the Animals from the file
+    }
+
+    private synchronized static int nextId() {
+        int id = nextId;
+        ++nextId;
+        return id;
+    }
+
+    private boolean save() throws IOException {
+        return false;
+    }
+
+    private void load() throws IOException {
+    }
 
     @Override
-    public Customer[] getCustomers() throws IOException {
+    public Customer[] getCustomers() {
         return new Customer[0];
     }
 
     @Override
-    public Customer[] findCustomers(String containsText) throws IOException {
+    public Customer[] findCustomers(String containsText) {
         return new Customer[0];
     }
 
     @Override
-    public Customer getCustomer(int id) throws IOException {
+    public Customer getCustomer(int id) {
         return null;
     }
 
     @Override
-    public Customer createCustomer(Customer customer) throws IOException {
+    public Customer createCustomer(Customer customer) {
         return null;
     }
 
     @Override
-    public Customer updateCustomer(Customer customer) throws IOException {
+    public Customer updateCustomer(Customer customer) {
         return null;
     }
 
     @Override
-    public boolean deleteCustomer(int id) throws IOException {
+    public boolean deleteCustomer(int id) {
         return false;
     }
 }
