0b84cfe2c8087d1f49b5020e0b234944d0d9647d
Krysta Lestico <krl4721@g.rit.edu>
2023-03-08 11:18:10 -0500

    Implemented shopping cart controller

--- estore-api/src/main/java/com/zoo/api/zooapi/controller/ShoppingCartController.java
+++ estore-api/src/main/java/com/zoo/api/zooapi/controller/ShoppingCartController.java
@@ -8,34 +8,92 @@ import com.zoo.api.zooapi.persistence.ShoppingCartDAO;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @RestController
-@RequestMapping("customers")
+@RequestMapping("shopppingcart")
 public class ShoppingCartController {
     private static final Logger Log = Logger.getLogger(ShoppingCartController.class.getName());
     private ShoppingCartDAO shoppingCartDao;
 
-    public ShoppingCartController(ShoppingCartDAO mockDAO) {
+    public ShoppingCartController(ShoppingCartDAO shoppingCartDao) {
         this.shoppingCartDao = shoppingCartDao;
     }
 
     @GetMapping("")
     public ResponseEntity<ShoppingCart[]> getShoppingCart() {
-        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
-    }
+        Log.info("GET /shoppingcart");
 
-    
+        try {
+            ShoppingCart cart[] = shoppingCartDao.getShoppingCarts();
+            return new ResponseEntity<ShoppingCart[]>(cart,HttpStatus.OK);
+        }
+        catch(IOException e) {
+            Log.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
     @PutMapping("/{id}")
     public ResponseEntity<ShoppingCart> addAnimalToShoppingCart(@PathVariable int id, @PathVariable int animalId) {
-        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+        Log.info("PUT /shoppingcart " + id);
+
+        // Replace below with your implementation
+        try {
+            ShoppingCart cartnew = shoppingCartDao.addAnimalToShoppingCart(id, animalId);
+            if (cartnew == null)
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            else {
+                return new ResponseEntity<>(cartnew, HttpStatus.OK);
+            }
+        }
+        catch(IOException e) {
+            Log.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @DeleteMapping()
-    public ResponseEntity<Boolean> removeShoppingCart(@PathVariable int id, @PathVariable int animalId) {
-        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    public ResponseEntity<Boolean> removeShoppingCart(@PathVariable int id) {    
+        Log.info("DELETE /cart/" + id);
+    
+        try {
+    
+            if (shoppingCartDao.deleteShoppingCart(id))
+                return new ResponseEntity<>(HttpStatus.OK);
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        catch(IOException e) {
+            Log.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ShoppingCart> removeAnimalFromShoppingCart(@PathVariable int id, @PathVariable int animalId) {
+        Log.info("DELETE /animals/" + animalId);
+
+        try {
+            ShoppingCart cartnew = shoppingCartDao.removeAnimalFromShoppingCart(id, animalId);
+            if (cartnew == null)
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            else {
+                return new ResponseEntity<>(cartnew, HttpStatus.OK);
+            }
+        }
+        catch(IOException e) {
+            Log.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
