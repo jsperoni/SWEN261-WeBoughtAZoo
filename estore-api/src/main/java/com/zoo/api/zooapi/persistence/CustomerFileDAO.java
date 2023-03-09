7daccba5b142b00db43105d6fb624a04c9e1f74a
Krysta Lestico <krl4721@g.rit.edu>
2023-03-09 13:25:53 -0500

    Implemented customerFileDao

--- estore-api/src/main/java/com/zoo/api/zooapi/persistence/CustomerFileDAO.java
+++ estore-api/src/main/java/com/zoo/api/zooapi/persistence/CustomerFileDAO.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.logging.Logger;
@@ -34,40 +35,101 @@ public class CustomerFileDAO implements CustomerDAO {
         return id;
     }
 
-    private boolean save() throws IOException {
-        return false;
+    private Customer[] getCustomerArray() {
+        return getCustomerArray(null);
     }
 
-    private void load() throws IOException {
+    private Customer[] getCustomerArray(String text){
+        ArrayList<Customer> CustomerArrayList = new ArrayList<>();
+
+        for (Customer customer : customers.values()){
+            if (text == null || customer.getUsername().contains(text)) {
+                CustomerArrayList.add(customer);
+            }
+        }
+
+        Customer[] CustomerArray = new Customer[CustomerArrayList.size()];
+        CustomerArrayList.toArray(CustomerArray);
+        return CustomerArray;
+    }
+
+    private boolean save() throws IOException {
+        Customer[] Customerarray = getCustomers();
+        objectMapper.writeValue(new File(filename), Customerarray);
+        return true;
+    }
+
+    private boolean load() throws IOException {
+        customers = new TreeMap<>();
+        nextId = 0;
+
+        Customer[] customerArray = objectMapper.readValue(new File(filename), Customer[].class);
+
+        for (Customer customer : customerArray){
+            customers.put(customer.getId(), customer);
+            if(customer.getId() > nextId)
+                nextId = customer.getId();
+        }
+
+        ++nextId;
+        return true;
     }
 
     @Override
     public Customer[] getCustomers() {
-        return new Customer[0];
+        synchronized(customers){
+            return getCustomerArray();
+        }
     }
 
     @Override
     public Customer[] findCustomers(String containsText) {
-        return new Customer[0];
+        synchronized(customers){
+            return getCustomerArray(containsText);
+        }
     }
 
     @Override
     public Customer getCustomer(int id) {
-        return null;
+        synchronized(customers) {
+            if (customers.containsKey(id)) 
+                return customers.get(id);
+            else
+                return null;
+        }
     }
 
     @Override
-    public Customer createCustomer(Customer customer) {
-        return null;
+    public Customer createCustomer(Customer customer) throws IOException {
+        synchronized(customers){
+            Customer newCustomer = new Customer(nextId, customer.getUsername(), customer.getPassword(), customer.getPersonal(), customer.getCard(), customer.getHistory());
+            customers.put(newCustomer.getId(), newCustomer);
+            save();
+            return newCustomer;
+        }
     }
 
     @Override
-    public Customer updateCustomer(Customer customer) {
-        return null;
+    public Customer updateCustomer(Customer customer) throws IOException {
+        synchronized(customers) {
+            if (customers.containsKey(customer.getId()) == false)
+                return null;
+
+            customers.put(customer.getId(), customer);
+            save();
+            return customer;
+        }
     }
 
     @Override
-    public boolean deleteCustomer(int id) {
-        return false;
+    public boolean deleteCustomer(int id) throws IOException {
+        synchronized(customers) {
+            if (customers.containsKey(id)) {
+                customers.remove(id);
+                return save();
+            }
+            else 
+                return false;
+        }
     }
 }
