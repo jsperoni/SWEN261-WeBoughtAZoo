61179a83c46f9110636706e667ce673b2a3d840e
Krysta Lestico <krl4721@g.rit.edu>
2023-03-20 15:37:17 -0400

    Generated animal component

--- estore-ui/angular-we_built_a_zoo/src/app/app.module.ts
+++ estore-ui/angular-we_built_a_zoo/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AnimalsComponent } from './animals/animals.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimalsComponent
   ],
   imports: [
     BrowserModule,
