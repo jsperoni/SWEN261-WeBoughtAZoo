55edd07fd5c52590f9c3726d1cc46e5e83ea85f5
jts9328 <jts9328@rit.edu>
2023-04-05 19:11:31 -0400

    added styling that works better for our website

--- estore-ui/angular-we_built_a_zoo/src/app/app-routing.module.ts
+++ estore-ui/angular-we_built_a_zoo/src/app/app-routing.module.ts
@@ -15,6 +15,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent},
+  { path: 'animals', component: AnimalsComponent},
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: AnimalDetailComponent },
   { path: 'history', component: HistoryComponent },
